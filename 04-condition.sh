a6e61cc1b96db4962569aceccfbe6bb71a9c5074
Manuel Viger <viger.manuel@gmail.com>
2018-12-03 10:41:30 -0500

    Done with conditions

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
+note=60
+
+# "test" au lieu de [[ x ]]
+# "then"
+if test $note -lt 60
+then
+	echo "Désolé, vous devez reprendre le cours..."
+elif [[ $note -eq 60  ]];then
+	echo "Vous êtes à la limite"
+else
+	echo "Bravo"
+fi
+
+read lettre
+
+case $lettre in 
+	[[:lower:]])
+		echo "minuscule"
+		;;
+	*)
+		echo "autre"
+		;;
+esac
 
 
-
-
+# != veut dire "Nest pas égal à..."
+# Pour dire "Est égal à...", il faut mettre "x = y" (un seul égal avec espaces de chaque côté)
+if test $lettre != "a"
+then
+	echo "La lettre n'est pas a"
+fi
 
 
 
