6b537998d6e84c3139fb5aa4ce638c646dc7d61f
Manuel Viger <viger.manuel@gmail.com>
2018-12-03 12:15:52 -0500

    Exemple de code qui appel un autre programme dans un while

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+echo ==================
+echo "CVM Soft"
+echo "d = Dictionnaire"
+echo "q = Quit"
+echo =================
+
+read choix
+
+while test $choix != q
+do
+	if test $choix = d
+	then
+		./exercice1.sh
+	elif test $choix != d -a $choix != q
+	then
+		echo "Vous devez saisir d ou q"
+	fi
+
+	sleep 2
+	clear
+	echo ==================
+	echo "CVM Soft"
+	echo "d = Dictionnaire"
+	echo "q = Quit"
+	echo =================
+	
+	read choix
+done
