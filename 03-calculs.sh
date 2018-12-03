8c46e57928f9bde670b293c5f2b1796158a76174
Manuel Viger <viger.manuel@gmail.com>
2018-12-03 10:09:38 -0500

    Done with calculations

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+
+# Affiche "10+19"
+echo 10+19
+# Effectue le calcul puis affiche le resultat
+echo $((10+19))
+
+read -p "Nombre 1 :" premier
+read -p "Nombre 2 :" second
+
+let resultat=$premier+$second
+
+echo $resultat
