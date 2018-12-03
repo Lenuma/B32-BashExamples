19a9185dd1a056325cdc22e45047c429e4056a73
Manuel Viger <viger.manuel@gmail.com>
2018-12-03 11:17:55 -0500

    Done with files

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# -e = Le fichier est-il existant
+# -d = Est-ce un répertoire
+# $1 = Premier argument passé
+
+if test -e $1
+then
+	echo "existe"
+
+	if test -d $1
+	then
+		echo "C'est un répertoire"
+	fi
+else
+	echo "inexistant"
+fi
