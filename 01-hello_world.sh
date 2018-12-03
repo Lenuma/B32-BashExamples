34224e498e442780138700720f133eb506a0584c
Manuel Viger <viger.manuel@gmail.com>
2018-12-03 09:50:08 -0500

    Done with Hello World

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+echo "Salut"
+
+# -n = empêche un retour de ligne
+echo -n "Vive"
+echo "Linux"
+
+# -e = permet l'introduction de caractères spéciaux
+echo -e "123\n123\n123"
+
+# read = cin en c++
+read -p "Vous avez quel âge?" age
+# Pour assigner une variable, pas de $. Pour utiliser une variable, pour l'appeler, il faut $
+echo $age "Ans! Vous paraissez plus jeune"
